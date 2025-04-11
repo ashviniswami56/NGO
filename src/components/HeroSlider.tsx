5ed8360461078e72ada1d0b5a63d373da20e819e
Ashvini Swami <ashviniswami56@gmail.com>
2025-04-11 15:53:30 +0530

    first commit

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,16 +1,18 @@
-
 'use client';
 
 import { useState, useEffect } from 'react';
 
-const HeroSlider = ({ backgroundImage }) => {
+type HeroSliderProps = {
+  backgroundImage?: string;
+};
+
+const HeroSlider = ({ backgroundImage }: HeroSliderProps) => {
   const images = [
     '/hero1.jpg',
     '/hero2.jpg',
     '/hero3.jpg',
     '/hero4.jpg',
     '/hero5.jpg'
-    // Add more image URLs as needed
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -18,7 +20,7 @@ const HeroSlider = ({ backgroundImage }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 7000); // Change image every 5 seconds
+    }, 7000);
 
     return () => clearInterval(interval);
   }, []);
@@ -27,12 +29,10 @@ const HeroSlider = ({ backgroundImage }) => {
 
   return (
     <div
-      className="relative  mx-[15px] h-[400px] md:h-[400px] lg:h-[500px] bg-cover bg-center flex items-center transition-all duration-1000"
+      className="relative mx-[15px] h-[400px] md:h-[400px] lg:h-[500px] bg-cover bg-center flex items-center transition-all duration-1000"
       style={{ backgroundImage: `url('${currentBackground}')` }}>
-      
     </div>
   );
 };
 
 export default HeroSlider;
-
